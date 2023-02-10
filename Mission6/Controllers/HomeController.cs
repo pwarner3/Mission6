aed46df96cedbd48384f38c4ec9b24890db8e8be
Parker Warner <plouwarner@gmail.com>
2023-02-09 18:19:08 -0700

    Add project files.

---
+++ Mission6/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MoviesContext _context { get; set; }
+
+        //Constructor
+        public HomeController(ILogger<HomeController> logger, MoviesContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddMovies()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddMovies(MovieModel movie)
+        {
+            _context.Add(movie);
+            _context.SaveChanges();
+
+            return View(movie);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission6/Models/MoviesContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6.Models
+{
+    public class MoviesContext :DbContext
+    {
+        //Constructor
+        public MoviesContext(DbContextOptions<MoviesContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MovieModel> Responses { get; set; }
+
+        /* To seed db with data */
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieModel>().HasData(
+                new MovieModel //one movie entry
+                {
+                    MovieId = 1,
+                    Title = "Star Wars III - Revenge of the Sith",
+                    Category = "Action/Sci-Fi",
+                    Year = 2005,
+                    Director = "George Lucas",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "Favorite Movie"
+                },
+                new MovieModel //one movie entry
+                {
+                    MovieId = 2,
+                    Title = "Superbad",
+                    Category = "Comedy/Teen",
+                    Year = 2007,
+                    Director = "Greg Mottola",
+                    Rating = "R",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "Funny and a little bad"
+                },
+                new MovieModel //one movie entry
+                {
+                    MovieId = 3,
+                    Title = "Thor: Ragnarok",
+                    Category = "Action/Sci-Fi",
+                    Year = 2017,
+                    Director = "Taika Waititi",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "Great superhero movie"
+                }
+           );
+        }
+    }
+}
